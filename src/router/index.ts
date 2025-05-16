67d982802badc4056ca8bcb9067498791dcd51c1
Scott Collins <scollinsse@gmail.com>
2025-05-16 15:19:54 -0500

    rename to twelvehourview

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CalendarView from '../views/CalendarView.vue'
+import TwelveHourView from '../views/TwelveHourView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'calendar',
-      component: CalendarView,
+      name: 'TwelveHourView',
+      component: TwelveHourView,
     },
   ],
 })
